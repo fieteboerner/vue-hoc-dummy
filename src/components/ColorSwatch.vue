a2d0b75eb7ede70c68fe874b4ae1c288dece9b86
Fiete Börner <fieteboerner@googlemail.com>
2019-04-09 22:16:39 +0200

    optimize color swatch

--- src/components/ColorSwatch.vue
+++ src/components/ColorSwatch.vue
@@ -33,19 +33,25 @@ export default {
 <style lang="scss" scoped>
     .color-swatch {
         padding: 1em;
-        box-shadow: 0 0 0 1px rgba(63, 63, 68, 0.05), 0 1px 3px 0 rgba(63, 63, 68, 0.15);
+        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+        transition: all 0.4s cubic-bezier(.25,.8,.25,1);
         overflow: hidden;
         font-size: 13px;
         border-radius: 3px;
 
+        &:hover {
+            box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+        }
+
         .color-swatch-preview {
-            margin: -1em -1em 1em;
+            margin: -1em -1em 0;
             height: 128px;
-            border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+            border-bottom: 1px solid rgba(0, 0, 0, 0.12);
         }
 
         .color-swatch-name {
-            margin-bottom: 0.4em;
+            margin-top: 0.75em;
+            margin-bottom: 0.3em;
         }
     }
 </style>
