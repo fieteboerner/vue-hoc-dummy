afd65060b915ec9b4e0bb080e5f1ea08e819b218
Fiete Börner <fieteboerner@googlemail.com>
2019-04-09 20:17:16 +0200

    add color swatch component

---
+++ src/components/ColorSwatch.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="color-swatch">
+        <div class="color-swatch-preview" :style="style"></div>
+        <h3 class="color-swatch-name">{{ name }}</h3>
+        <div>{{ color }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ColorSwatch',
+    props: {
+        name: {
+            type: String,
+            reqired: true,
+        },
+        color: {
+            type: String,
+            required: true,
+        },
+    },
+    computed: {
+        style() {
+            return {
+                'background-color': this.color,
+            };
+        },
+    },
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .color-swatch {
+        padding: 1em;
+        box-shadow: 0 0 0 1px rgba(63, 63, 68, 0.05), 0 1px 3px 0 rgba(63, 63, 68, 0.15);
+        overflow: hidden;
+        font-size: 13px;
+        border-radius: 3px;
+
+        .color-swatch-preview {
+            margin: -1em -1em 1em;
+            height: 128px;
+            border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+        }
+
+        .color-swatch-name {
+            margin-bottom: 0.4em;
+        }
+    }
+</style>
+
+<docs>
+Source: https://vueds.com/example/#/Design%20Tokens
+
+</docs>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,67 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div class="swatches">
+      <ColorSwatch v-for="color in colors" :name="color.name" :color="color.color" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import ColorSwatch from '@/components/ColorSwatch.vue';
 
 export default {
   name: 'home',
-  components: {}
+  components: { ColorSwatch },
+  computed: {
+    colors() {
+      return [
+        { name: '$color-red', color: 'rgb(235, 59, 36)'},
+        { name: '$color-yellow', color: 'gb(255, 186, 10)'},
+        { name: '$color-blue', color: 'rgb(37, 138, 239)'},
+        { name: '$color-cloud', color: 'rgb(233, 243, 253)'},
+        { name: '$color-grey', color: 'rgb(127, 136, 145)'},
+        { name: '$color-white', color: 'rgb(255, 255, 255)'},
+
+        { name: '$color-red', color: 'rgb(235, 59, 36)'},
+        { name: '$color-yellow', color: 'gb(255, 186, 10)'},
+        { name: '$color-blue', color: 'rgb(37, 138, 239)'},
+        { name: '$color-cloud', color: 'rgb(233, 243, 253)'},
+        { name: '$color-grey', color: 'rgb(127, 136, 145)'},
+        { name: '$color-white', color: 'rgb(255, 255, 255)'},
+
+        { name: '$color-red', color: 'rgb(235, 59, 36)'},
+        { name: '$color-yellow', color: 'gb(255, 186, 10)'},
+        { name: '$color-blue', color: 'rgb(37, 138, 239)'},
+        { name: '$color-cloud', color: 'rgb(233, 243, 253)'},
+        { name: '$color-grey', color: 'rgb(127, 136, 145)'},
+        { name: '$color-white', color: 'rgb(255, 255, 255)'},
+
+        { name: '$color-red', color: 'rgb(235, 59, 36)'},
+        { name: '$color-yellow', color: 'gb(255, 186, 10)'},
+        { name: '$color-blue', color: 'rgb(37, 138, 239)'},
+        { name: '$color-cloud', color: 'rgb(233, 243, 253)'},
+        { name: '$color-grey', color: 'rgb(127, 136, 145)'},
+        { name: '$color-white', color: 'rgb(255, 255, 255)'},
+
+        { name: '$color-red', color: 'rgb(235, 59, 36)'},
+        { name: '$color-yellow', color: 'gb(255, 186, 10)'},
+        { name: '$color-blue', color: 'rgb(37, 138, 239)'},
+        { name: '$color-cloud', color: 'rgb(233, 243, 253)'},
+        { name: '$color-grey', color: 'rgb(127, 136, 145)'},
+        { name: '$color-white', color: 'rgb(255, 255, 255)'},
+      ];
+    },
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .swatches {
+    display: grid;
+    grid-gap: 1em;
+    grid-template-columns: repeat(auto-fit, minmax(225px, 1fr));
+  }
+</style>
+
